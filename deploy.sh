4688cbae872959a92784c83dcb7cb67154223b5e
Gagandeep Kang <gskang@bu.edu>
2021-09-07 20:42:33 -0400

    finish deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 export FLASK_APP=project.server
 export FLASK_ENV=development
-export APP_SETTINGS="project.server.config.DevelopmentConfig"
+export APP_SETTINGS="project.server.config.ProductionConfig"
 flask db init
 flask db migrate
 flask db upgrade
